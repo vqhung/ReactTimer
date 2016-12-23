4a4525e7eb5f37f38e0fc67c3446701bff074140
Hung Vo <vqhung@gmail.com>
2016-12-23 21:03:51 +0700

    Inital commit

---
+++ app/components/Main.jsx
@@ -0,0 +1,21 @@
+var React = require('react');
+var Nav = require('Nav');
+
+
+var Main = (props) => {
+  return (
+    <div>
+      <Nav/>
+      <div>
+        <div>
+          <p>Main.jsx render</p>
+          {props.children}
+        </div>
+      </div>
+
+    </div>
+
+  );
+};
+
+module.exports = Main;
